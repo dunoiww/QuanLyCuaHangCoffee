b6f10796fcd80595834155a383717dd4b9bbe301
Nam Ngo <namngo102003@gmail.com>
2023-11-25 16:44:25 +0700

    ui dashboard

---
+++ QuanLyChuoiCuaHangCoffee/QuanLyChuoiCuaHangCoffee/ViewModel/AdminVM/DashboardVM/DashboardViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Library.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyChuoiCuaHangCoffee.ViewModel.AdminVM.DashboardVM
+{
+    public class DashboardViewModel : BaseViewModel
+    {
+
+    }
+}
--- QuanLyChuoiCuaHangCoffee/QuanLyChuoiCuaHangCoffee/ViewModel/AdminVM/MainAdminViewModel.cs
+++ QuanLyChuoiCuaHangCoffee/QuanLyChuoiCuaHangCoffee/ViewModel/AdminVM/MainAdminViewModel.cs
@@ -1,14 +1,25 @@
 ﻿using Library.ViewModel;
+using QuanLyChuoiCuaHangCoffee.Views.Admin.DashboardPage;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace QuanLyChuoiCuaHangCoffee.ViewModel.AdminVM
 {
     public class MainAdminViewModel : BaseViewModel
     {
+        public ICommand LoadStatisticalPageML { get; set; }
 
+        public MainAdminViewModel()
+        {
+            LoadStatisticalPageML = new RelayCommand<Frame>((p) => { return true; }, (p) =>
+            {
+                p.Content = new MainDashboardPage();
+            });
+        }
     }
 }
